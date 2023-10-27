20a1b4d88f3f338cad61db36f1a9276d19bb4fb0
Michael Cavallaro <michaelcavallaro03@gmail.com>
2023-10-27 17:22:37 -0600

    chore: update podfile.lock

--- ios/RNCommandBar.swift
+++ ios/RNCommandBar.swift
@@ -3,9 +3,9 @@ import CommandBarIOS
 
 @objc(RNCommandBar)
 class RNCommandBar : NSObject {
-  
     @objc
     func openHelpHub(_ orgId: NSString, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-        CommandBar.openHelpHub(orgId: orgId as String)
+        CommandBar.openHelpHub(orgId: orgId as String, resolve: nil, reject: nil)
+        resolve(true)
     }
 }
